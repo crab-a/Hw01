54db3bbe8ac1b3e297d7b8b1333c66a1cc2bcc78
crab-a <crab@notmailinator.com>
2021-04-07 16:12:09 +0300

    started Statistics

--- Statistics.py
+++ Statistics.py
@@ -1,5 +1,17 @@
-import math
+from math import ceil
 
 
-def sum(values):
-    
+class Statistics:
+
+    def sum(values):
+
+        total = 0
+        for num in values:
+            total += num
+        return total
+
+    def mean(values):
+
+        total = sum(values)
+        length = len(values)
+        return (total/length)
